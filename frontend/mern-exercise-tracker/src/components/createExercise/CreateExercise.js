8dc6ad767bd2cd9205a4cefe80b70c7a9c05dd70
Anthon Wellsjö <anthonwellsjo@gmail.com>
2020-10-19 10:08:09 +0200

    commit

--- frontend/mern-exercise-tracker/src/components/createExercise/CreateExercise.js
+++ frontend/mern-exercise-tracker/src/components/createExercise/CreateExercise.js
@@ -1,17 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useHistory } from 'react-router';
+import DatePicker from 'react-datepicker';
+import axios from 'axios';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default function () {
+
+    const history = useHistory();
 
     const [exercise, setExercise] = useState({ username: "", description: "", duration: 0, date: new Date(), users: [] });
 
     const onChangeUsername = e => {
-        setExercise(prev => ({ ...prev, username: e.target.value }))
+        const value = e.target.value;
+        setExercise(prev => ({ ...prev, username: value }))
     }
-    const onChangedDescription = e => {
-        setExercise(prev => ({ ...prev, description: e.target.value }))
+    const onChangeDescription = e => {
+        const value = e.target.value;
+        setExercise(prev => ({ ...prev, description: value }))
     }
     const onChangeDuration = e => {
-        setExercise(prev => ({ ...prev, duration: e.target.value }))
+        const value = e.target.value;
+        setExercise(prev => ({ ...prev, duration: value }))
     }
     const onChangeDate = date => {
         setExercise(prev => ({ ...prev, username: date }))
@@ -22,8 +32,13 @@ export default function () {
 
         const newExercise = exercise;
         console.log("new exc", newExercise);
-        window.location("/");
+        axios.post("http://localhost:5000/exercises/add", exercise)
+            .then(res => console.log(res.data))
+            .catch(err => console.log("Error: " + err));
+        history.push("/");
     }
+
+    const userInput = useRef();
 
     useEffect(() => {
         setExercise(prev => ({ ...prev, username: "test user", users: ["test user"] }))
@@ -36,14 +51,46 @@ export default function () {
                 <div className="form-group">
                     <label>Username: </label>
                     <select
-                        ref="userInput"
+                        ref={userInput}
                         required
                         className="form-control"
                         value={exercise.username}
-                        onChange={e => onChangeUsername(e)}
-                    >
-                        
+                        onChange={onChangeUsername}
+                    >{
+                            exercise.users.map(u => {
+                                return <option
+                                    key={u}
+                                    value={u}>
+                                    {u}
+                                </option>
+                            })
+                        }
                     </select>
+                </div>
+                <div className="form-group">
+                    <label>Duration (in minutes): </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={exercise.duration}
+                        onChange={onChangeDuration}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Date: </label>
+                    <div>
+                        <DatePicker
+                            selected={exercise.date}
+                            onChange={onChangeDate}
+                        />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <input
+                        type="submit"
+                        value="Create Exercise Log"
+                        className="btn btn-primary"
+                    />
                 </div>
             </form>
         </div>
--- frontend/mern-exercise-tracker/src/components/createUser/CreateUser.js
+++ frontend/mern-exercise-tracker/src/components/createUser/CreateUser.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 export default function () {
-    return(
-        <h1>
-            create user
-        </h1>
+
+    const [username, setUsername] = useState("");
+
+    const onChangeUsername = e => {
+        const value = e.target.value;
+        setUsername(value);
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        
+        const user = {username: username};
+        console.log(username);
+
+        axios.post('http://localhost:5000/users/add', user)
+            .then(res => console.log(res.data))
+            .catch(err => console.log("Error: " + err))
+
+
+        setUsername("");
+    }
+
+    return (
+
+        <div>
+            <h3>
+                Create New User
+            </h3>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label>Username: </label>
+                    <input
+                        type="text"
+                        required
+                        className="form-control"
+                        value={username}
+                        onChange={onChangeUsername}
+                    />
+                </div>
+                <div className="form-group">
+                    <input type="submit" value="Create User" className="btn btn-primary" />
+                </div>
+            </form>
+        </div>
     )
 };
